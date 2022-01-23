eb091893dee833ab8e40d355dff21cf5a25500f5
Olja Priyakovich <oljap.@oljas-air.attlocal.net>
2022-01-23 16:08:23 -0500

    adding packages and classes

--- Expense Reimbursement API/src/expenseReimbursement/Driver.java
+++ Expense Reimbursement API/src/expenseReimbursement/Driver.java
@@ -1,10 +1,16 @@
 package expenseReimbursement;
 
+import expenseReimbursement.models.createTicket;
+
 public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		createTicket testTicket = new createTicket();
+		System.out.println(testTicket);
 	}
+	
 
 }
--- Expense Reimbursement API/src/expenseReimbursement/models/createTicket.java
+++ Expense Reimbursement API/src/expenseReimbursement/models/createTicket.java
@@ -1,5 +1,58 @@
 package expenseReimbursement.models;
 
 public class createTicket {
+	private String title;
+	private String description;
+	private int id;
+	
 
+	
+	public createTicket() {
+			
+	}
+	
+	public createTicket(String title, String description) {
+		this.title = title;
+		this.description = description;
+	}
+	
+	public createTicket (String title, String description, int id)
+	{
+		this(title, description);
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "createTicket [title=" + title + ", description=" + description + ", id=" + id + "]";
+	}
+	
+	
+	
 }
+	
+

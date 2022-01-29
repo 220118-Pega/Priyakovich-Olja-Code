fc1c016a72a9a48ec66794b151193b4c6b197256
Olja Priyakovich <oljap.@oljas-air.attlocal.net>
2022-01-29 15:20:53 -0500

    not sure what i'm fixing any more :alien:

--- Project - 0/src/ui/MainMenu.java
+++ Project - 0/src/ui/MainMenu.java
@@ -4,10 +4,14 @@ import java.io.ObjectInputFilter.Status;
 import java.time.LocalDate;
 import java.util.Scanner;
 import Models.Ticket;
+import bl.ITicketBl;
+import bl.TicketBL;
+
 
 
 public class MainMenu {
 	private Scanner myscanner;
+	private ITicketBl ticketBL;
 	public MainMenu (Scanner myscanner) {
 		this.myscanner = myscanner;
 	
@@ -25,16 +29,15 @@ public class MainMenu {
 			String userInput = myscanner.nextLine();
 			switch (userInput) {
 			case "1":
-				System.out.println("creating ticket...");
 				createTicket();
 			case "2":
-				System.out.println("view my ticket");
+				viewMyTickets();
 				break;
 			case "3":
-				System.out.println("view all tickets");
+				viewAllTickets();
 				break;
 			case "4":
-				System.out.println("view ticket my id");
+				viwTicketsByid();
 				break;
 			default:
 				System.out.println("Sorry wrong input, please try again");
@@ -76,30 +79,86 @@ public class MainMenu {
 		}
 	
 		Ticket newTicket = new Ticket(Status.pending, amount, category, LocalDate.now(),employee_id);
-		
-		
+		ticketBL.addTicket(newTicket);
+		System.out.println(newTicket);
 		
 	}
 	
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	private void viewMyTickets() {
+		// TODO Auto-generated method stub
+		System.out.println("Enter employee id number:");
+		int employee_id = Integer.parseInt(myscanner.nextLine());
+		for(Ticket ticket: ticketBL.getTicketByEmployeeId(employee_id)) {
+				System.out.println(ticket);
+			}
+	}
+	
+	private void viewAllTickets() {
+		// TODO Auto-generated method stub
+		for(Ticket ticket: ticketBL.getTicket()) {
+			System.out.println(ticket);
+		}
+		System.out.println("Filter tickets: [A] Approved, [P] Pending, [R] Rejected, [X] Go Back");
+		String select = myscanner.nextLine().toLowerCase();
+		switch (select) {
+		case "a":
+			for(Ticket ticket: ticketBLfilterStatus(Status.approved)) {
+				System.out.println(ticket);
+			}
+			break;
+		case "p":
+			for(Ticket ticket: ticketBLfilterStatus(Status.pending)) {
+				System.out.println(ticket);
+			}
+			break;
+		case "r":
+			for(Ticket ticket: ticketBLfilterStatus(Status.rejected)) {
+				System.out.println(ticket);
+			}
+			break;
+		case "x":
+			System.out.println("Return to Main Menu");
+			break;
+		default:
+			System.out.println("Sorry wrong input, please try again");
+		}
+		
+	}
+	
+	private void viwTicketsByid() {
+		// TODO Auto-generated method stub
+		System.out.println("Select ticket by id:");
+		for(Ticket ticket: ticketBL.getTicket()) {
+			System.out.println(ticket);
+		}
+		int select = Integer.parseInt(myscanner.nextLine());
+		Ticket selectedTicket;
+		try {
+			selectedTicket = ticketBL.getTicketById(select);
+			System.out.println(selectedTicket);
+			System.out.println("Update Status: [A] Accept, [R]Rejected, [X]Cancel");
+			String userInput = myscanner.nextLine().toLowerCase();
+			switch (userInput) {
+			case "a":
+				selectedTicket.setStatus(Status.approved);
+				ticketBL.updateTicket(selectedTicket);
+				System.out.println("Ticket Approved!");
+				break;
+			case "r":
+				selectedTicket.setStatus(Status.rejected);
+				ticketBL.updateTicket(selectedTicket);
+				System.out.println("Ticket rejected!");
+				break;
+			case "x":
+				System.out.println("Return to Main Menu");
+			default:
+				break;
+			}
+				
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+			
+	}
 
